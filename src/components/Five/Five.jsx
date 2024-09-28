e84db6e896be32d92684d4d0d25b40518bdb6544
RUDRA PATEL <rudrapatel892005@gmail.com>
2024-09-28 17:25:21 +0530

    Add files via upload

---
+++ src/components/Five/Five.jsx
@@ -0,0 +1,16 @@
+import "./five.css";
+
+import React from "react";
+
+const Five = () => {
+  return <div className="fi">
+      
+      <div className="footer">
+          <p className="digi">Let's get physical or keep it digital</p>
+          <h1 className="get-in">Get in touch</h1>
+      </div>
+      <a href="" className="credit">@ &nbsp; &nbsp;   &nbsp; &nbsp;   &nbsp; &nbsp;Instagram</a>
+  </div>;
+};
+
+export default Five;
---
+++ src/components/Third/Third.jsx
@@ -0,0 +1,52 @@
+import "./third.css";
+import C from "../img/volvo.jpg";
+import D from "../img/cup.jpg";
+import E from "../img/drink.jpg";
+import F from "../img/man.jpg";
+import React from "react";
+import { LottiePlayer } from "@lottiefiles/lottie-player";
+
+const Third = () => {
+  return <div className="t">
+      <div className="rec">Recent <br /> Cases</div>
+      <div className="rec-1">
+          <h1 className="title-1">Volunteers Helping</h1>
+          <p className="des-1"> in the Places that needs the most Help.</p>
+          <div className="sub-rec-1">
+              <img src={C} alt=""  className="picdrop"/>
+          </div>
+      </div>
+      <div className="rec-2">
+          <h1 className="title-1">Our Approach at Connecting</h1>
+          <p className="des-1">An Interactive Website fit for all platforms.</p>
+          <div className="sub-rec-1">
+              <img src={D} alt=""  className="picdrop"/>
+          </div>
+      </div>
+      <div className="rec-3">
+          <h1 className="title-1">Our Approach at Connecting</h1>
+          <p className="des-1">An Interactive Website fit for all platforms.</p>
+          <div className="sub-rec-1">
+              <img src={E} alt=""  className="picdrop"/>
+          </div>
+      </div>
+      <div className="rec-4">
+          <h1 className="title-1">Rainer Schönfelder</h1>
+          <p className="des-1">A website relaunch that goes deeper</p>
+          <div className="sub-rec-1">
+              <img src={F} alt=""  className="picdrop"/>
+          </div>
+      </div>
+      <div className="oval-2">
+      <lottie-player
+        autoplay
+        loop
+        mode="normal"
+        src="https://assets2.lottiefiles.com/packages/lf20_avndu8kw.json"
+        >
+    </lottie-player>
+    </div>
+  </div>;
+};
+
+export default Third;
